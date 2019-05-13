209a254ea013ee591243d36d390edaa13f5cd5d9
Yauhen Zhechka <Yauhen_Zhechka@epam.com>
2019-05-13 12:25:47 +0300

    feat: add directive for hide order button in students table

---
+++ src/app/common/directives/button-order-hide.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appButtonOrderHide]'
+})
+export class ButtonOrderHideDirective {
+  constructor(private element: ElementRef, private renderer: Renderer2) {}
+  @HostListener('mouseenter') onMouseEnter() {
+    console.log('ON element');
+    this.changeOpacity(1);
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.changeOpacity(0);
+  }
+  private changeOpacity(n: number) {
+    this.renderer.setStyle(
+      this.element.nativeElement.querySelector('.table__button'),
+      'opacity',
+      n
+    );
+  }
+}
